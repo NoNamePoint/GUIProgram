f37a457af847217641d7b7876fd605f70546f8b4
NoNamePoint <krassergey-37@mail.ru>
2020-08-18 22:17:44 +0300

    add validation empty field, add search widget

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ class Main(tk.Frame):
         self.add_img = tk.PhotoImage(file="images/add.png")
         self.update_img = tk.PhotoImage(file="images/update.png")
         self.delete_img = tk.PhotoImage(file='images/delete.png')
-
+        self.search_img = tk.PhotoImage(file="images/search.png")
+        
         toolbar = tk.Frame(bg = "#d7d8e0", bd = 5)
         toolbar.pack(side="top", fill="x")
         
@@ -34,6 +35,10 @@ class Main(tk.Frame):
                                  compound='top', relief='flat', font=('Verdana', 8), bg="#d7d8e0")
         deleteButton.pack(side="left", padx = 15)
 
+        searchButton = tk.Button(toolbar, text=u"Найти", command=lambda: self.open_search(), image=self.search_img,
+                                 compound='top', relief='flat', font=('Verdana', 8), bg="#d7d8e0")
+        searchButton.pack(side="left", padx=15)
+
         self.tree = ttk.Treeview(root, columns=('ID', 'description', 'form','total'), height=10, show='headings', selectmode='browse')
         self.tree.column('ID', width=25, anchor='center')
         self.tree.column('description', width=355, anchor='center')
@@ -46,10 +51,13 @@ class Main(tk.Frame):
         self.tree.heading('total', text="Сумма")
         self.tree.pack()
 
-    def records(self, *fields):
-        self.__db.insert_data(*fields)
-        self.view_records()
-
+    def add_records(self, *fields):
+        """Функция выполняет валидацию поля на пустое значение, добавляет данные в БД и выводит значения в виджет treeview(см. функцию view_record)"""
+        if all(fields): 
+            self.__db.insert_data(*fields)
+            self.view_records()
+        else:
+            messagebox.showerror(u'Error', message=u'Заполните поля')
     
     def update_record(self, *fields):
         index = self.tree.set(self.tree.selection()[0], '#1')
@@ -58,11 +66,12 @@ class Main(tk.Frame):
 
 
     def view_records(self):
+        """Функция посылает запрос в БД, получает значения полей и выводит значения в виджет treeview"""
         [self.tree.delete(i) for i in self.tree.get_children()]
         for row in self.__db.cursor.execute('''SELECT * FROM homeexpances'''):
             self.tree.insert('', 'end', values=row)
 
-    def set_record(self, entry_expance, rvbar, entry_expance_sum):
+    def set_record_to_vidgets(self, entry_expance, rvbar, entry_expance_sum):
         description=self.tree.set(self.tree.selection()[0], '#2') #собираем значения из полей
         form = self.tree.set(self.tree.selection()[0], '#3')
         cost = self.tree.set(self.tree.selection()[0], '#4')
@@ -79,7 +88,7 @@ class Main(tk.Frame):
             element = self.tree.selection()[0]
             index = self.tree.set(self.tree.selection()[0], '#1')
         except:
-            messagebox.showerror(u'Error',message=u'Выберите поле для удаления')   
+            messagebox.showerror(u'Error', message=u'Выберите поле для удаления')   
         if element:
             answer = messagebox.askyesno(u'Подтверждение удаления', message=u'Вы точно хотите удалить запись?')   
         if answer:
@@ -91,7 +100,17 @@ class Main(tk.Frame):
         AddExpance()
 
     def open_updateexpance(self):
-        UpdateExpance()
+        index = None
+        try:
+            index = self.tree.set(self.tree.selection()[0], '#1')
+        except:
+            messagebox.showerror(title="Пустое поле",
+                                 message="Вы не выбрали запись")
+        if index:
+            UpdateExpance()
+
+    def open_search(self):
+        Search()
 
     
 # дочернее окно
@@ -121,7 +140,7 @@ class AddExpance(tk.Toplevel):
                                                    u'Такси', 
                                                    u'Транспорт', 
                                                    u'Здоровье', 
-                                                   u'Разное'],  width=17)
+                                                   u'Разное'], state= "readonly" , width=17)
         entry_expense_type1 = tk.Radiobutton(
             self, text=u'Доход', value=u'Доход', variable=self.rvbar)
         entry_expense_type2 = tk.Radiobutton(self, text=u'Расход', value=u'Расход', variable = self.rvbar)
@@ -132,9 +151,9 @@ class AddExpance(tk.Toplevel):
         entry_expense_type2.place(x=210, y=80)
         self.entry_expense_sum.place(x=150, y=110)
 
-        self.button_accept = tk.Button(self, text=u"Добавить", command= lambda: self.view.records(self.entry_expense.get(),
-                                                                                                  self.rvbar.get(),
-                                                                                                  self.entry_expense_sum.get()))
+        self.button_accept = tk.Button(self, text=u"Добавить", command= lambda: self.view.add_records(self.entry_expense.get(),
+                                                                                                      self.rvbar.get(),
+                                                                                                      self.entry_expense_sum.get()))
         self.button_accept.place(x = 100, y = 150)
         self.button_cancel = tk.Button(self, text = u"Отменить", command = self.destroy)
         self.button_cancel.place(x = 200,  y = 150)
@@ -149,11 +168,23 @@ class UpdateExpance(AddExpance):
         super().__init__()
         self.title(u"Редактировать запись")
         self.button_accept.destroy()
-        self.view.set_record(self.entry_expense, self.rvbar, self.entry_expense_sum)
+        self.view.set_record_to_vidgets(self.entry_expense, self.rvbar, self.entry_expense_sum)
         button_update = tk.Button(self, text=u"Изменить", command=lambda: self.view.update_record(self.entry_expense.get(),
                                                                                                    self.rvbar.get(),
                                                                                                    self.entry_expense_sum.get()))
         button_update.place(x=100, y=150)
+
+
+class Search(tk.Toplevel):
+    def __init__(self):
+        super().__init__(root)
+        self.title(u"Поиск")
+        self.geometry('400x220+400+300')
+        self.resizable(False, False)
+        search_field_name = tk.Label(self, text=u"Что ищем?")
+        search_field_name.place (x = 175, y = 50)
+        search_field = tk.Entry(self)
+        search_field.place(x= 150, y=70)
 
 
 if __name__ == "__main__":
